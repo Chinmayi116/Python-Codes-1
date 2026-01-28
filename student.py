4b366c9e994b59e732c43a410c3ba69506ae7423
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-01-28 18:28:00 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print("Name:", self.name)
+        print("Marks:", self.marks)
+
+s = Student("Chinmayi", [85, 90, 88])
+s.display()
+
