884d9f19cddf61cc2152d823f43a5c1985ad1395
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-02-26 11:41:20 +0530

    Create Numpy2.py

---
+++ Numpy2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array([[1, 2], 
+              [3, 4]])
+
+B = np.array([[5, 6], 
+              [7, 8]])
+
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+
+print("Matrix Addition:\n", A + B)
+print("Matrix Multiplication:\n", np.dot(A, B))
