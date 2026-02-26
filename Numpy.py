55eb378a0abc0791a678f04001cd123c21b199e3
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-02-26 11:40:05 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Creating arrays
+a = np.array([10, 20, 30, 40])
+b = np.array([1, 2, 3, 4])
+
+print("Array A:", a)
+print("Array B:", b)
+
+# Arithmetic operations
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
