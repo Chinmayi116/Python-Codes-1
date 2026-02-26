c5452985c4122d30631bd151ae6b803ab1513eac
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-02-26 11:41:52 +0530

    Create Numpy3.py

---
+++ Numpy3.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+print("Zeros Array:\n", np.zeros((2,3)))
+print("Ones Array:\n", np.ones((2,3)))
+print("Identity Matrix:\n", np.eye(3))
+print("Random Numbers:\n", np.random.rand(2,2))
