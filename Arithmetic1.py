08dbc0353800f79acb58d87f3f2c6e1d1de51617
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-04-12 17:58:54 +0530

    Implement basic arithmetic operations in Arithmetic1.py

---
+++ Arithmetic1.py
@@ -0,0 +1,17 @@
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number:))
+add = a + b
+sub = a - b
+mul = a * b
+
+if b != 0:
+    div = a / b
+else:
+    div = "Not possible (division by zero)"
+
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div) mm
